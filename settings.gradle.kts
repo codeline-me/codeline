4bc9bb9cca7cde47d33d8df1daf20234c7f156f1
Malek Hijazi <malek.hijazii@gmail.com>
2024-05-18 16:50:08 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Codeline"
+include(":app")
+include(":auth")
+include(":data")
+include(":ui")
+include(":core")
+include(":analytics")
+include(":localization")
+include(":payments")
+include(":notifications")
